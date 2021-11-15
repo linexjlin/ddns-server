55573aedf6505bb4a0c7e6e9f839267750b9cf23
line <line.xj.lin@gmail.com>
2021-11-15 05:43:53 +0000

    auto get ip form http request

--- api.go
+++ api.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"strings"
 )
+
+func clientPublicIP(r *http.Request) string {
+	var ip string
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		ip = strings.TrimSpace(ip)
+		if ip != "" {
+			return ip
+		}
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
 
 func UpdateDNS(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	domain, ip := r.FormValue("domain"), r.FormValue("ip")
+	if ip == "" {
+		ip = clientPublicIP(r)
+	}
+
+	if domain == "" || ip == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	if err := service.CloudFlareA(domain, ip); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
